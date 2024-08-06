3e0e1d4605a148e3309bb9fdaf9d99f0268e1f0f
Amireddy Vinay Kumar Reddy <vinay@geoiq.io>
2024-08-06 15:05:47 +0530

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [visitorId, setVisitorId] = useState("");
+
+  useEffect(() => {
+    async function fetchFingerprint() {
+      const res = await fetch("/api/fingerprint");
+      if (res.ok) {
+        const data = await res.json();
+        setVisitorId(data.visitorId);
+      } else {
+        setVisitorId("Blocked");
+      }
+    }
+
+    fetchFingerprint();
+  }, []);
+
+  return (
+    <div>
+      <h1>Fingerprint Test</h1>
+      <p>Visitor ID: {visitorId}</p>
+    </div>
+  );
+}
